807872dda400b4367482d85d691c9ee483c1d01f
utkarsh tiwari <utkarsh2313003@akgec.ac.in>
2025-01-28 03:30:06 +0530

    done

--- src/components/applications/ShowcaseExplorer.tsx
+++ src/components/applications/ShowcaseExplorer.tsx
@@ -23,12 +23,12 @@ const ShowcaseExplorer: React.FC<ShowcaseExplorerProps> = (props) => {
             left={56}
             width={initWidth}
             height={initHeight}
-            windowTitle="Henry Heffernan - Showcase 2022"
+            windowTitle="Utkarsh Tiwari - Portfolio 2025"
             windowBarIcon="windowExplorerIcon"
             closeWindow={props.onClose}
             onInteract={props.onInteract}
             minimizeWindow={props.onMinimize}
-            bottomLeftText={'© Copyright 2022 Henry Heffernan'}
+            bottomLeftText={'© Copyright 202 Utkarsh Tiwari'}
         >
             <Router>
                 <div className="site-page">
